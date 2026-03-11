0c5d73760e5044a164b0f538a1cc8613bf7e541d
hassan12mohamed <hassan2004153@gmail.com>
2026-03-12 01:06:48 +0200

    update usepopcorn app

---
+++ src/commponent/Box.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+export default function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+/*
+function WatchedBox({ children }) {
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && <>{children} </>}
+    </div>
+  );
+}
+  */
